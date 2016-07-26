abf94913a556c44a6219afd55a947c25eda1464d
yubin <2445449433@qq.com>
2016-07-26 11:33:21 +0800

    添加页面跳转

--- app/src/main/java/com/yu/gittest/MainActivity.java
+++ app/src/main/java/com/yu/gittest/MainActivity.java
@@ -1,18 +1,35 @@
 package com.yu.gittest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnTestYu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Log.i("","首页");
+
+        initView();
     }
+
+    private void initView() {
+        btnTestYu = (Button) findViewById(R.id.btnTestYu);
+        btnTestYu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,TestYuActivity.class));
+            }
+        });
+    }
+
 
     private void firtMethod(){
         Log.i("master！！","方法一");
